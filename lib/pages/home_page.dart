7f3e40371c62c5e9df1e32dbd87c00c788edcf6d
FlutLab.io <>
2022-06-11 07:37:03 +0000

    Autosave

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ class Homepage extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
+        Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Pulsa", style: TextStyle(fontSize: 17)))
         Padding(
           padding: const EdgeInsets.only(top: 25),
           child: Row(
@@ -71,6 +72,24 @@ class Homepage extends StatelessWidget {
                       MaterialButton(
                         onPressed: () {},
                         color: Colors.white,
+                        textColor: Colors.blue,
+                        child: Icon(
+                          Icons.gamepad,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Game", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
                         textColor: Colors.red,
                         child: Icon(
                           Icons.money_outlined,
@@ -85,6 +104,103 @@ class Homepage extends StatelessWidget {
             ],
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.red,
+                        child: Icon(
+                          Icons.health_and_safety,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Asuransi", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.blue,
+                        child: Icon(
+                          Icons.money,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Investasi", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.green,
+                        child: Icon(
+                          Icons.person,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("BPJS", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.green,
+                        child: Icon(
+                          Icons.shop_2,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Ecomrsc", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.blue,
+                        child: Icon(
+                          Icons.all_inclusive_outlined,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10.0), child: Text("Lainnya", style: TextStyle(fontSize: 17)))
+                    ],
+                  )),
+            ],
+          ),
+        )
       ],
     ));
   }
