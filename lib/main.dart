226a7064e6d65517122cfeb63b699464c5e26f5f
FlutLab.io <>
2022-06-11 06:30:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:projectuas/routes/main_routes.dart';
+import 'package:projectuas/pages/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(homepage());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
