b2b00b9972be292df9d4902dc95a4778e1f7a592
FlutLab.io <>
2022-06-11 11:57:11 +0000

    Autosave

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -106,7 +106,7 @@ class Homepage extends StatelessWidget {
           ),
         ),
         Padding(
-            padding: const EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.only(top: 25),
             child: Text(
               "Daftar Tugas",
               style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
