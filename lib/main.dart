c7c24135be4be9a8bba622d365bb7ad41440964c
Dava Adriansyah <themepeck.cli@gmail.com>
2022-06-19 02:07:34 +0700

    jadikan onboarding sebagai halaman pertama

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projectuas/pages/main_pages.dart';
+import 'package:projectuas/pages/onboarding_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: OnboardingPage(),
     );
   }
 }
