75503f08fb347f6f369cc8e1a1c5ef38a8144872
Dava Adriansyah <themepeck.cli@gmail.com>
2022-06-20 00:38:02 +0700

    menbahkan tema baru di themes

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -4,7 +4,7 @@ Color primaryYellowColor = Color(0xfffff500);
 Color primaryOrangeColor = Color(0xffF7AA00);
 Color primaryDarkBlueColor = Color(0xff235784);
 Color primaryLightBlueColor = Color(0xFF40A8C4);
-Color primaryRedBlueColor = Color(0xFFFF4B4B);
+Color primaryRedColor = Color(0xFFFF4B4B);
 
 Color textBlack = Color(0xff222222);
 Color textGrey = Color(0xff94959b);
