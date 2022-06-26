6066df36902991331781b5d75b60c2deee685db4
Dava Adriansyah <themepeck.cli@gmail.com>
2022-06-27 02:31:38 +0700

    menghapus import home_page

--- lib/routes/main_routes.dart
+++ lib/routes/main_routes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:projectuas/pages/absensi_pages.dart';
-import 'package:projectuas/pages/home_page.dart';
 import 'package:projectuas/pages/login_page.dart';
 import 'package:projectuas/pages/main_pages.dart';
 import 'package:projectuas/pages/onboarding_page.dart';
