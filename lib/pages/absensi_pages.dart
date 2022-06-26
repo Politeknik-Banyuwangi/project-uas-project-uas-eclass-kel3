b4f5cdc3ecb92d5098b31ba2003fe5bf590e185f
Dava Adriansyah <themepeck.cli@gmail.com>
2022-06-27 02:52:06 +0700

    commint

--- lib/pages/absensi_pages.dart
+++ lib/pages/absensi_pages.dart
@@ -9,14 +9,23 @@ class AbsensiPages extends StatelessWidget {
             alignment: Alignment.center,
             margin: EdgeInsets.all(50),
             child: Column(children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 6,
+              ),
               Image.asset('assets/gambar_absen.png'),
               Text(
                 'Yukk Absen!',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: primaryYellowColor),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: primaryYellowColor),
               ),
               Text(
                 'Scan QR Code yang ada pada website \n dan wholaa kamu berhasil absen!',
-                style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal, color: Colors.grey),
+                style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey),
               ),
               SizedBox(
                 height: 44,
@@ -25,7 +34,9 @@ class AbsensiPages extends StatelessWidget {
                   borderRadius: BorderRadius.circular(30),
                   child: Container(
                     height: 56,
-                    decoration: BoxDecoration(color: primaryYellowColor, borderRadius: BorderRadius.circular(30)),
+                    decoration: BoxDecoration(
+                        color: primaryYellowColor,
+                        borderRadius: BorderRadius.circular(30)),
                     child: Material(
                       color: Colors.transparent,
                       child: InkWell(
@@ -34,7 +45,10 @@ class AbsensiPages extends StatelessWidget {
                         child: Center(
                           child: Text(
                             'Mulai Absen!',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800, color: Colors.black),
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.black),
                           ),
                         ),
                       ),
